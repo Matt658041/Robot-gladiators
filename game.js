5dba70ee7a87beaf7cfd3786a0576dbc41b8da5f
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-02-18 14:04:27 -0600

    script

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const playerName = window.prompt (`What is your robots name?`);
 let playerHealth =100;
 let playerAttack = 10;
@@ -15,11 +17,24 @@ const fight = function (){
 
   // Log a resulting message to the console so we know that it worked.
   console.log(playerName + `attacked` + enemyName + `.`  + enemyName + `now has` + enemyHealth + `health remaining.`);
+  //check enemy's health
+  if (enemyHealth <= 0) {
+    windows.alert(enemyName + `has died!`);
+  } else {
+    window.alert(enemyName + `still has` + enemyHealth + `health left.`);
+  }
 
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
   playerHealth = playerHealth - enemyAttack;
 
   // Log a resulting message to the console so we know that it worked.
   console.log(enemyName + `attacked` + playerName + `.` + playerName + `now has`) + playerHealth + `health remaining.`
+  //check players health
+  if (playerHealth <= 0) {
+    window.alert(playerName + `has died.`);
+      } else {
+        window.alert(playerName + `still has` + playerHealth + `health left.`);
+      }
 };
 fight();
+
