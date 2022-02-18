5f61a69b9bbaae6d4481cfb407dccf4bb4008b70
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-02-18 12:46:02 -0600

    updated js

--- game.js
+++ game.js
@@ -1,1 +1,25 @@
-window.alert("This is an alert! Javascript is running")
+const playerName = window.prompt (`What is your robots name?`);
+let playerHealth =100;
+let playerAttack = 10;
+
+const enemyName = `Roborto`;
+let enemyHealth =50;
+let enemyAttack = 12;
+
+
+const fight = function (){
+    // Alert players that they are starting the round.
+    window.alert(`Welcome to Robot Gladiators!`);
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
+
+  // Log a resulting message to the console so we know that it worked.
+  console.log(playerName + `attacked` + enemyName + `.`  + enemyName + `now has` + enemyHealth + `health remaining.`);
+
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+  playerHealth = playerHealth - enemyAttack;
+
+  // Log a resulting message to the console so we know that it worked.
+  console.log(enemyName + `attacked` + playerName + `.` + playerName + `now has`) + playerHealth + `health remaining.`
+};
+fight();
